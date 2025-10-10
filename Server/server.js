00e5a3e222a9067925732fd447a70e0418132020
Rishika-108 <rishikathakur334@gmail.com>
2025-10-10 12:06:47 +0530

    Database Intialisation and Backend Setup

---
+++ Server/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./Config/db.js";
+
+dotenv.config();
+
+const app = express();
+//Connect Database
+connectDB();
+
+app.use(cors());
+app.use(express.json()); 
+
+//API Endpoint to connect to user
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
